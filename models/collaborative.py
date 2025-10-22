25c5cc0965ac425fa16e031d78cbcf79d6c2dd97
Hamilton <hamiltonmussi.cf@gmail.com>
2025-10-22 10:02:54 -0300

    create collaborative notebook

--- models/collaborative.py
+++ models/collaborative.py
@@ -4,6 +4,7 @@ from typing import List
 from scipy.sparse import csr_matrix
 from implicit.als import AlternatingLeastSquares
 from models.base import BaseRecommender
+from utils.geo_filter import GeoFilter
 
 
 class CollaborativeFilteringRecommender(BaseRecommender):
@@ -12,12 +13,14 @@ class CollaborativeFilteringRecommender(BaseRecommender):
         n_factors: int,
         regularization: float,
         iterations: int,
-        random_state: int
+        random_state: int,
+        geo_top_k: int
     ):
         self.n_factors = n_factors
         self.regularization = regularization
         self.iterations = iterations
         self.random_state = random_state
+        self.geo_top_k = geo_top_k
 
         self.model = None
         self.user_to_idx = None
@@ -26,15 +29,20 @@ class CollaborativeFilteringRecommender(BaseRecommender):
         self.idx_to_event = None
         self.interaction_matrix = None
         self.train = None
+        self.events = None
+        self.geo_filter = None
 
-    def fit(self, R: np.ndarray, W: np.ndarray, user_to_idx: dict, event_to_idx: dict, train: pd.DataFrame):
+    def fit(self, R: np.ndarray, W: np.ndarray, user_to_idx: dict, event_to_idx: dict, train: pd.DataFrame, events: pd.DataFrame):
         self.user_to_idx = user_to_idx
         self.event_to_idx = event_to_idx
         self.idx_to_user = {i: u for u, i in user_to_idx.items()}
         self.idx_to_event = {i: e for e, i in event_to_idx.items()}
         self.train = train
+        self.events = events
 
-        interaction_weighted = (R * W).T
+        self.geo_filter = GeoFilter(events, train)
+
+        interaction_weighted = R * W
         self.interaction_matrix = csr_matrix(interaction_weighted)
 
         self.model = AlternatingLeastSquares(
@@ -51,25 +59,28 @@ class CollaborativeFilteringRecommender(BaseRecommender):
         if user_idx is None:
             return []
 
-        if exclude_seen:
-            seen_events = set(self.train[self.train["user"] == user_id]["event"])
-            filter_items = [self.event_to_idx[e] for e in seen_events if e in self.event_to_idx]
-        else:
-            filter_items = []
-
-        recommendations = self.model.recommend(
-            user_idx,
-            self.interaction_matrix[user_idx],
-            N=n + len(filter_items),
-            filter_already_liked_items=False
+        candidate_events = self.geo_filter.get_nearby_events(
+            user_id,
+            top_k=self.geo_top_k,
+            exclude_seen=exclude_seen
         )
 
-        event_indices = [idx for idx, _ in recommendations]
+        valid_candidates = []
+        valid_indices = []
+        for event in candidate_events:
+            if event in self.event_to_idx:
+                event_idx = self.event_to_idx[event]
+                valid_candidates.append(event)
+                valid_indices.append(event_idx)
 
-        if exclude_seen:
-            filter_set = set(filter_items)
-            event_indices = [idx for idx in event_indices if idx not in filter_set]
+        if not valid_indices:
+            return []
 
-        event_indices = event_indices[:n]
+        user_factor = self.model.user_factors[user_idx]
+        event_factors = self.model.item_factors[valid_indices]
 
-        return [self.idx_to_event[idx] for idx in event_indices]
+        scores = event_factors.dot(user_factor)
+
+        top_positions = np.argsort(scores)[::-1][:n]
+
+        return [valid_candidates[pos] for pos in top_positions]
